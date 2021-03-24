33bb0ba7d3759772c01df738ccd6f88dab81f24e
christianturner <christiandavidphoto@gmail.com>
2021-03-24 14:28:32 -0400

    total income prop passed to Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,57 @@
 <template>
-  <h2>Hello</h2>
+  <!-- 2) passing total income as a prop -->
+  <Header :totalIncome="state.totalIncome" />
 </template>
 
-<script></script>
+<script>
+import { reactive, computed } from "vue";
+import Header from "./components/Header";
 
-<style></style>
+export default {
+  setup() {
+    const state = reactive({
+      income: [
+        {
+          value: 400,
+        },
+        {
+          value: 500,
+        },
+      ],
+      totalIncome: computed(() => {
+        let temp = 0;
+
+        // making sure we actually have income
+        if (state.income.length > 0) {
+          // we'll loop through the array of income amounts
+          for (let i = 0; i < state.income.length; i++) {
+            // and add them up for a total, assigned to temp
+            temp += state.income[i].value;
+          }
+        }
+
+        return temp;
+      }),
+    });
+
+    // 1) what we return here will be usable in our templates
+    return {
+      Header,
+      state,
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+}
+
+body {
+  background: #eee;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1>Income Tracker</h1>
+    <!-- 4) we create a template with our passed down total income prop value -->
+    <div class="total-income">${{ totalIncome }}</div>
+  </header>
+</template>
+
+<script>
+export default {
+  // 3) receiving the total income prop passed from our App component
+  // declaring its type a Number
+  props: {
+    totalIncome: Number,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 30px;
+  background-color: #313131;
+  border-bottom: 5px solid #ffce00;
+}
+header h1 {
+  color: #eee;
+  font-size: 28px;
+}
+header .total-income {
+  font-family: "Fira Code", "Fira Sans", sans-serif;
+  background-color: #ffce00;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 900;
+  padding: 5px 10px;
+  min-width: 100px;
+  text-align: center;
+  border-radius: 8px;
+  box-shadow: inset 0px 0px 6px rgba(0, 0, 0, 0.25);
+  text-shadow: 0px 3px 3px rgba(0, 0, 0, 0.25);
+}
+</style>
