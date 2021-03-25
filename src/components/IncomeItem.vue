ad5bf9dc067a09a7eaa0a2ce4a62428f81135fd3
christianturner <christiandavidphoto@gmail.com>
2021-03-25 12:43:16 -0400

    formatted date displayed

--- src/components/IncomeItem.vue
+++ src/components/IncomeItem.vue
@@ -3,7 +3,7 @@
     <div class="removeItem" @click="removeItem">x</div>
     <div class="desc">{{ income.desc }}</div>
     <div class="price">$ {{ income.value }}</div>
-    <div class="date">{{ income.date }}</div>
+    <div class="date">{{ formattedDate }}</div>
   </div>
 </template>
 
@@ -11,6 +11,15 @@
 export default {
   props: {
     income: Object,
+  },
+  setup(props) {
+    let date = new Date(props.income.date);
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let formattedDate = month + "/" + day + "/" + year;
+
+    return { formattedDate };
   },
 };
 </script>
